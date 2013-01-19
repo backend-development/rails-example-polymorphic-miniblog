4ae3db950bc8235467a122cec82e0aebb1bfd3ba
Brigitte Jellinek <brigitte.jellinek@fh-salzburg.ac.at>
2013-01-19 13:55:43 +0100

    firs version of edit form: nested form, just for url

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -58,9 +58,8 @@ class ArticlesController < ApplicationController
   # PUT /articles/1.json
   def update
     @article = Article.find(params[:id])
-
     respond_to do |format|
-      if @article.update_attributes(params[:article])
+      if  @article.update_attributes(params[:article])
         format.html { redirect_to @article, notice: 'Article was successfully updated.' }
         format.json { head :no_content }
       else
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
-  attr_accessible :title
+  attr_accessible :title, :attachment_attributes
 
   belongs_to :attachment, :polymorphic => true
+  accepts_nested_attributes_for :attachment
 end
